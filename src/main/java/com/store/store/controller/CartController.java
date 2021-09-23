fbc642397a52fc3aea8feedba1eb464971a809b3
Alhusseinr <Ramialhussein98@gmail.com>
2021-09-23 12:58:24 -0700

    mhm

--- src/main/java/com/store/store/controller/CartController.java
+++ src/main/java/com/store/store/controller/CartController.java
@@ -8,6 +8,7 @@ import com.store.store.model.Item;
 import com.store.store.repository.CartItemsRepository;
 import com.store.store.repository.CartRepository;
 import com.store.store.repository.ItemRepository;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
@@ -18,10 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
@@ -108,20 +106,69 @@ public class CartController {
         try {
             List<CartItems> cartItems = cartItemsRepository.findByCartId(id);
             ArrayList<Item> items = new ArrayList<>();
+            HashMap<UUID, Integer> itemsInCart = new HashMap();
             double total = 0;
 
-            for(int i = 0; i < cartItems.size(); i++) {
-                items.add(itemRepository.findById(cartItems.get(i).getItemId()).get());
+            for(CartItems cartItem : cartItems) {
+                Item fetchItem = itemRepository.findById(cartItem.getItemId()).get();
+                if(!itemsInCart.containsKey(fetchItem.getId())) {
+                    itemsInCart.put(itemRepository.findById(cartItem.getItemId()).get().getId(), 1);
+                } else if(itemsInCart.containsKey(fetchItem.getId())) {
+                    itemsInCart.put(
+                            itemRepository.findById(cartItem.getItemId()).get().getId(),
+                            itemsInCart.get(itemRepository.findById(cartItem.getItemId()).get().getId()) + 1
+                    );
+                }
             }
 
-            for(Item item : items) {
-                total += item.getItemPrice();
+            for(UUID key : itemsInCart.keySet()) {
+                Item fetchedItem = itemRepository.findById(key).get();
+                Item it = new Item();
+
+                it.setId(fetchedItem.getId());
+                it.setItemName(fetchedItem.getItemName());
+                it.setItemPrice(fetchedItem.getItemPrice());
+                it.setItemQuantity(itemsInCart.get(key));
+                it.setItemDescription(fetchedItem.getItemDescription());
+
+                items.add(it);
             }
 
-            return ResponseEntity.ok(items);
+            for(Item item: items) {
+                if(item.getItemQuantity() == 1) {
+                    total += item.getItemPrice();
+                }else if(item.getItemQuantity() > 1) {
+                    total += item.getItemPrice() * item.getItemQuantity();
+                }
+            }
+
+            System.out.println(total);
+
+            JSONObject obj = new JSONObject();
+            obj.put("items", items);
+            obj.put("total", total);
+
+            return ResponseEntity.ok(obj.toString());
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private ArrayList<Item> updateItemQty(ArrayList<Item> items, UUID itemId, HashMap<UUID, Integer> map) {
+
+        for(Item item : items) {
+            System.out.println(item.getId() == itemId);
+            if(item.getId() == itemId) {
+                item.setItemQuantity(map.get(item.getId()));
+                System.out.println("inside loop");
+                System.out.println(item);
+            }
+        }
+
+        System.out.println("inside update");
+        System.out.println(items);
+
+        return items;
     }
 }
